4651a00b798c9a6a9cc1a926d5b2f76384cdc2d3
Pablocstlpz <pablomanueldcl93@gmail.com>
2026-04-15 19:35:56 +0200

    feat: agregar componente 'ApartadoServicios' con su plantilla y estilos

---
+++ src/app/components/apartado-servicios/apartado-servicios.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-apartado-servicios',
+  imports: [RouterLink],
+  templateUrl: './apartado-servicios.html',
+  styleUrl: './apartado-servicios.css',
+})
+export class ApartadoServicios {}
