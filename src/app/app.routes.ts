b91f1346444f292af6bf048725297d1d337b679c
Pablo M. Del Castillo López <pmcastillo@deuser.es>
2026-04-15 14:59:18 +0200

    feat: agregar enlace a la política de privacidad en el pie de página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/layout/main/main').then((m) => m.Main),
+  },
+  {
+    path: 'politica-privacidad',
+    loadComponent: () =>
+      import('./components/footerLinks/politica-privacidad/politica-privacidad').then(
+        (m) => m.PoliticaPrivacidad,
+      ),
+  },
+];
---
+++ src/app/components/footerLinks/politica-privacidad/politica-privacidad.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-politica-privacidad',
+  imports: [],
+  templateUrl: './politica-privacidad.html',
+  styleUrl: './politica-privacidad.css',
+})
+export class PoliticaPrivacidad {}
