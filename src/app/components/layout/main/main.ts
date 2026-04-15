c2de8bde0c20c40d6f9637eb4049f1d13a876de0
Pablocstlpz <pablomanueldcl93@gmail.com>
2026-04-15 19:36:32 +0200

    feat: agregar identificadores a secciones y enlaces de navegación en el componente principal

--- src/app/components/layout/main/main.ts
+++ src/app/components/layout/main/main.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
-export class Main {}
+export class Main {
+
+  
+}
