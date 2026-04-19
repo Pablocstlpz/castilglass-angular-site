c85a6ba5a5b62bfe7121f530ead045ab35828b54
Pablocstlpz <pablomanueldcl93@gmail.com>
2026-04-19 22:58:53 +0200

    feat: agregar componente 'AvisoLegal' con su plantilla y estilos

---
+++ src/app/components/footerLinks/aviso-legal/aviso-legal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aviso-legal',
+  imports: [],
+  templateUrl: './aviso-legal.html',
+  styleUrl: './aviso-legal.css',
+})
+export class AvisoLegal {}
