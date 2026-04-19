be53a9e21feea17b7e54473760c2af3df4c30beb
Pablocstlpz <pablomanueldcl93@gmail.com>
2026-04-19 22:59:01 +0200

    feat: agregar componente 'PoliticaCookies' con su plantilla y estilos

---
+++ src/app/components/footerLinks/politica-cookies/politica-cookies.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-politica-cookies',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './politica-cookies.html',
+  styleUrl: './politica-cookies.css',
+})
+export class PoliticaCookies {}
